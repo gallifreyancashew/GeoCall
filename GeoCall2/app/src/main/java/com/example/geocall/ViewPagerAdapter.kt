497c661eede78dcfb6ce8977ce50b1f70db66c2c
Wyli Olivete <azeptoblade@gmail.com>
2020-09-28 17:02:45 +0800

    added homepage

--- GeoCall2/app/src/main/java/com/example/geocall/ViewPagerAdapter.kt
+++ GeoCall2/app/src/main/java/com/example/geocall/ViewPagerAdapter.kt
@@ -5,26 +5,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class ViewPagerAdapter(private var title: List<String>, private var details: List<String>, private var images: List<Int>) : RecyclerView.Adapter<ViewPagerAdapter.Pager2ViewHolder>(){
+class ViewPagerAdapter(private var title: List<String>, private var details: List<String>, private var image: List<Int>) : RecyclerView.Adapter<ViewPagerAdapter.Pager2ViewHolder>() {
 
     inner class Pager2ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val itemTitle: TextView = itemView.findViewById(R.id.title)
-        val itemDetails: TextView = itemView.findViewById(R.id.details)
-        val itemImage: ImageView = itemView.findViewById(R.id.image)
 
-//        init {
-//            itemImage.setOnClickListener{ v: View ->
-//                val position = adapterPosition
-//                Toast.makeText(itemView.context, "You clicked on item #${position+1}", Toast.LENGTH_SHORT).show()
-//            }
-//        }
+        val itemTitle: TextView = itemView.findViewById(R.id.onboarding_title)
+        val itemDetails: TextView = itemView.findViewById(R.id.onboarding_details)
+        val itemImage: ImageView = itemView.findViewById(R.id.onboarding_image)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Pager2ViewHolder {
-        return Pager2ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.activity_welcome_page1, parent, false))
+        return Pager2ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.activity_welcome_page, parent, false))
     }
 
     override fun getItemCount(): Int {
@@ -33,8 +27,8 @@ class ViewPagerAdapter(private var title: List<String>, private var details: Lis
 
     override fun onBindViewHolder(holder: Pager2ViewHolder, position: Int) {
         holder.itemTitle.text = title[position]
-        holder.itemDetails.text = title[position]
-        holder.itemImage.setImageResource(images[position])
+        holder.itemDetails.text = details[position]
+        holder.itemImage.setImageResource(image[position])
     }
 
 }
--- GeoCall2/app/src/main/java/com/example/geocall/WelcomePages.kt
+++ GeoCall2/app/src/main/java/com/example/geocall/WelcomePages.kt
@@ -2,14 +2,14 @@ package com.example.geocall
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager.widget.ViewPager
 import kotlinx.android.synthetic.main.activity_welcome_pages.*
 import me.relex.circleindicator.CircleIndicator3
 
 class WelcomePages : AppCompatActivity() {
 
     private var titlesList = mutableListOf<String>()
-    private var descList = mutableListOf<String>()
+    private var detailsList = mutableListOf<String>()
     private var imagesList = mutableListOf<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,31 +18,33 @@ class WelcomePages : AppCompatActivity() {
 
         postToList()
 
-        view_pager2.adapter = ViewPagerAdapter(titlesList,descList,imagesList)
-        view_pager2.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        view_pager2.adapter = ViewPagerAdapter(titlesList, detailsList, imagesList)
 
         val indicator = findViewById<CircleIndicator3>(R.id.indicator)
         indicator.setViewPager(view_pager2)
     }
 
-    private fun addToList(title:String, description:String, image:Int) {
+    private fun addToList(title: String, details: String, image:Int) {
         titlesList.add(title)
-        descList.add(description)
+        detailsList.add(details)
         imagesList.add(image)
     }
 
     private fun postToList() {
-        addToList("Welcome to",
+        addToList("Welcome",
             "Swipe to get started!",
             R.drawable.geocall_embossed)
         addToList("All-in-One Hotline Service",
-            "GeoCall compiles different hotline services like police stations, hospitals, suicide and crisis intervention, and many mor ethat are near your location.\\n\\nYou can also add personal emergency contacts later on in Settings.",
-            R.drawable.iphone)
+            "GeoCall compiles different hotline services like police stations, hospitals, suicide and crisis intervention," +
+                    " and many more that are near your location.\n\nYou can also add personal emergency contacts later on in Settings.",
+            R.drawable.i_phone)
         addToList("Geographically-based",
-            "Based on your location, GeoCall can look through the database for the nearest appropriate hotline services which you can choose from.\\n\\nYou also set the application to automatically call the nearest station instead in Settings.",
+            "Based on your location, GeoCall can look through the database for the nearest appropriate hotline services which" +
+                    "you can choose from.\n\nYou also set the application to automatically call the nearest station instead in Settings.",
             R.drawable.geographically_based)
         addToList("Shake to Activate",
-            "In the event of emergencies, GeoCall can be set to be activated through shaking the phone. Upon activation, GeoCall lists the numbers of nearby stations in a numerical order from which you can choose from.",
+            "In the event of emergencies, GeoCall can be set to be activated through shaking the phone. Upon activation," +
+                    " GeoCall lists the numbers of nearby stations in a numerical order from which you can choose from.",
             R.drawable.shake_to_activate)
     }
 }
